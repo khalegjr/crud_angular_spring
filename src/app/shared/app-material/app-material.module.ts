8a22249011b855566ccd6b7a232983acdb5374e7
Khaled <khalegjr@gmail.com>
2023-05-11 14:54:26 -0300

    atualiza angular cdk e material para versão 15

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
-import { MatCardModule } from "@angular/material/card";
+import { MatLegacyCardModule as MatCardModule } from "@angular/material/legacy-card";
 import { MatIconModule } from "@angular/material/icon";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { MatSnackBarModule } from "@angular/material/snack-bar";
-import { MatTableModule } from "@angular/material/table";
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from "@angular/material/legacy-progress-spinner";
+import { MatLegacySnackBarModule as MatSnackBarModule } from "@angular/material/legacy-snack-bar";
+import { MatLegacyTableModule as MatTableModule } from "@angular/material/legacy-table";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
